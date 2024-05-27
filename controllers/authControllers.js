80a89016a021d867069341b27327f962c7be0d48
Dikens27 <kar.karovich321@gmail.com>
2024-05-27 21:16:45 +0300

    create mail file

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,8 +1,10 @@
 import HttpError from "../helpers/HttpError.js";
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
+import crypto from "node:crypto";
 import jwt from "jsonwebtoken";
 import gravatar from "gravatar";
+import mail from "../mail/mail.js";
 
 export async function register(req, res, next) {
   const { email, password } = req.body;
@@ -16,14 +18,24 @@ export async function register(req, res, next) {
 
     const passwordHash = await bcrypt.hash(password, 10);
     const generatedAvatar = gravatar.url(email);
+    const verifyToken = crypto.randomUUID();
 
     const newUser = await User.create({
       email,
       password: passwordHash,
       avatarURl: generatedAvatar,
+      verifyToken,
     });
 
     const { subscription } = newUser;
+
+    await mail.sendMail({
+      to: email,
+      from: "kar.karovich321@gmail.com",
+      subject: "Welcome to Phone book",
+      html: `To confirm your email please click on the <a href="http://localhost:3000/api/users/verify/${verifyToken}">link</a>`,
+      text: `To confirm your email please open the link http://localhost:3000/api/users/verify/${verifyToken}`,
+    });
 
     res.status(201).send({ user: { email, subscription } });
   } catch (error) {
---
+++ mail/mail.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import nodemailer from "nodemailer";
+
+const { MAILTRAP_USERNAME, MAILTRAP_PASSWORD } = process.env;
+
+const transport = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: MAILTRAP_USERNAME,
+    pass: MAILTRAP_PASSWORD,
+  },
+});
+
+function sendMail(message) {
+  return transport.sendMail(message);
+}
+
+export default { sendMail };
